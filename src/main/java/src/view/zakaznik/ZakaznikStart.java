df7b55c647e65955274cee94e8ea707d04996731
sedlasi1 <sedlasi1@fel.cvut.cz>
2016-04-14 16:17:12 +0200

    Zalozeny balicky pro GUI

---
+++ src/main/java/src/view/zakaznik/ZakaznikStart.java
@@ -0,0 +1,32 @@
+package src.view.zakaznik;
+
+/**
+ * Created by root on 14.4.16.
+ */
+import javax.swing.*;
+
+public class ZakaznikStart {
+
+    public static void main(String [] args){
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("HelloWorldSwing");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Add the ubiquitous "Hello World" label.
+        JLabel label = new JLabel("Hello World");
+        frame.getContentPane().add(label);
+
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+}
